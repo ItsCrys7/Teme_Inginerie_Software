d49a4604d3103cc6ae9debec932c611bbcf0023a
Crys <cristian.toanca@emanuel.ro>
2025-11-20 20:53:14 +0200

    + 1 order, and new arrangement

--- Template_pattern/PizzaShop.java
+++ Template_pattern/PizzaShop.java
@@ -5,10 +5,14 @@ public class PizzaShop {
         Pizza myPizza = new PizzaPeperoni();
         myPizza.doPizza();
 
-        System.out.println(); 
+        // System.out.println(); 
 
-        System.out.println("--- Order 2: Veggy ---");
+        System.out.println("\n--- Order 2: Veggy ---");
         Pizza yourPizza = new PizzaVeggy();
         yourPizza.doPizza();
+
+        System.out.println("\n--- Order 3: With Cheese ---");
+        Pizza hisPizza = new PizzaWithCheese();
+        hisPizza.doPizza();
     }
 }
